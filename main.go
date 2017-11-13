9fe05269148689ba22d9b315b6cd56ffb23b3b90
01deyishu <01deyishu@gmail.com>
2017-11-13 09:14:45 +0800

    update main.go to httpserver v0.1

--- main.go
+++ main.go
@@ -2,33 +2,19 @@ package main
 
 import (
 	"fmt"
-	"net"
+	"log"
+	"net/http"
 )
 
-func main() {
-	fmt.Println("Starting the server ...")
-	listener, err := net.Listen("tcp", "0.0.0.0:9007")
-	if err != nil {
-		fmt.Println("Error listenling", err.Error())
-	}
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println("Error Accepting", err.Error())
-		}
-		go doServerStuff(conn)
-	}
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Inside HelloServer Handler")
+	fmt.Fprintf(w, "Hello, "+req.URL.Path[1:])
 }
 
-func doServerStuff(conn net.Conn) {
-	for {
-		buf := make([]byte, 1024)
-		info, err := conn.Read(buf)
-		if err != nil {
-			fmt.Println("Error readint", err.Error())
-			return
-		}
-		fmt.Printf("Received data: %v", string(buf[:info]))
+func main() {
+	http.HandleFunc("/", HelloServer)
+	err := http.ListenAndServe("0.0.0.0:9001", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err.Error())
 	}
 }
