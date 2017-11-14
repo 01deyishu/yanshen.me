975168ffbfdef50cfba60504c63810ad06ea9566
01deyishu <01deyishu@gmail.com>
2017-11-14 22:43:12 +0800

    nothing

--- main.go
+++ main.go
@@ -4,23 +4,24 @@ import (
 	"fmt"
 	"net"
 	"net/http"
-	//	"reflect"
+	//"reflect"
 )
 
 func SimpleServer(w http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(w, ".")
 }
 
-func main() {
+func GetAddr() {
 	nic, _ := net.Interfaces()
-	//fmt.Println(reflect.TypeOf(nic))
 	for i := 0; i < len(nic); i++ {
 		addr, _ := nic[i].Addrs()
-		if addr != nil {
-			fmt.Printf("NIC name is : %s , Address is %s\n", nic[i].Name, addr)
+		if len(addr) != 0 {
+			fmt.Printf("NIC name is : %s, Address is %s\n", nic[i].Name, addr)
 		}
-		//fmt.Println(reflect.TypeOf(nic[i]))
 	}
+}
+
+func main() {
 	http.HandleFunc("/", SimpleServer)
 	err := http.ListenAndServe("0.0.0.0:9007", nil)
 	if err != nil {
