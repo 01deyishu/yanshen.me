c606f106c282c505defb0cdf8f6a42c284ccc7eb
01deyishu <01deyishu@gmail.com>
2018-01-29 18:33:38 +0800

    增加信息输出函数

--- httpstaticfile.go
+++ httpstaticfile.go
@@ -2,12 +2,31 @@ package main
 
 import (
 	"fmt"
+	"github.com/vishvananda/netlink"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
+func httpInfo() {
+	fmt.Println("输入下列链接访问静态文件服务器：")
+	links, _ := netlink.LinkList()
+	for _, link := range links {
+		ip, _ := netlink.AddrList(link, 0)
+		if ip != nil {
+			url := strings.Split(ip[0].String(), "/")
+			isOk, _ := regexp.MatchString("127.*", url[0])
+			if !isOk {
+				fmt.Println("http://" + url[0])
+			}
+		}
+	}
+}
+
 func main() {
+	httpInfo()
 	http.Handle("/", http.FileServer(http.Dir(".")))
-	err := http.ListenAndServe(":9007", nil)
+	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		fmt.Errorf("%v", err)
 	}
